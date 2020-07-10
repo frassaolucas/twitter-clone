a6691b3b86332cbf58bdf3d693e0913df99404b8
Lucas Frassao <frassao.lucas@gmail.com>
2020-07-10 09:15:35 -0300

    started creating the sidebar

---
+++ src/components/MenuBar/styles.ts
@@ -0,0 +1,81 @@
+import styled, { css } from 'styled-components';
+
+import {
+  Home,
+  Notifications,
+  Email,
+  FavoriteBorder,
+  Person,
+  ExitToApp,
+  Rocketseat,
+} from '../../styles/icons';
+
+export const Container = styled.div`
+  display: none;
+
+  @media (min-width: 500px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    position: sticky;
+    top: 0;
+    left: 0;
+
+    padding: 0px 19px 20px;
+
+    max-height: 100vh;
+    overflow-y: auto;
+  }
+`;
+
+export const Topside = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (min-width: 1280px) {
+    align-items: flex-start;
+  }
+`;
+
+export const Logo = styled(Rocketseat)`
+  width: 41px;
+  height: 41px;
+
+  margin-bottom: 20px;
+
+  > path {
+    fill: var(--twitter);
+  }
+`;
+
+export const MenuButton = styled.div``;
+
+const iconCSS = css`
+  flex-shrink: 0;
+
+  width: 30px;
+  height: 30px;
+  color: var(--white);
+`;
+
+export const HomeIcon = styled(Home)`
+  ${iconCSS}
+`;
+
+export const BellIcon = styled(Notifications)`
+  ${iconCSS}
+`;
+
+export const EmailIcon = styled(Email)`
+  ${iconCSS}
+`;
+
+export const FavoriteIcon = styled(FavoriteBorder)`
+  ${iconCSS}
+`;
+
+export const ProfileIcon = styled(Person)`
+  ${iconCSS}
+`;
