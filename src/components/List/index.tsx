c300fc1aa23ada4d478ffee2f22a4b3a719870de
Lucas Frassao <frassao.lucas@gmail.com>
2020-07-11 15:46:11 -0300

    creating sideBar's list -- errors

---
+++ src/components/List/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Container, Item, Title } from './styles';
+
+interface Props {
+  title: string;
+  elements: React.ReactNode;
+}
+
+const List: React.FC<Props> = ({ title, elements }: Props) => {
+  return (
+    <Container>
+      <Item>
+        <Title>{title}</Title>
+      </Item>
+
+      {elements.map(element => (
+        <Item key={element}>{element}</Item>
+      ))}
+    </Container>
+  );
+};
+
+export default List;
